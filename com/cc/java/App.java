63292834eeff4bdcf1df77069cae3552ff94910f
Peter <schmidt.multimedia@gmail.com>
2024-01-31 10:27:37 +0100

    3. Iteration Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -14,28 +14,14 @@ public class App {
 
     }
 
-    // ---- 2. Iteration Overloading -----//
-
-    private static void pollObj(Queen obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("-----------------");
-    }
-
-    private static void pollObj(Worker obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("-----------------");
-    }
-
-    private static void pollObj(Drone obj){
+    // ---- 3. Iteration Polymorphie -----//
+    private static void pollObj(HoneyBee obj){
         output(obj.doYourJob());
         output(obj.fly());
         output("-----------------");
     }
 
 
-   
     private static void output(String outputStr) {
         System.out.println(outputStr);
     }
